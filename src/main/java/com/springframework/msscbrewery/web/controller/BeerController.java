9618ce41d62699be2510cb10e161c8527b766a27
siddharthashankarblore <sonumca04@gmail.com>
2019-08-28 11:20:48 +0530

    request mapping, test class for BeerController.

--- src/main/java/com/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/springframework/msscbrewery/web/controller/BeerController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.config.YamlProcessor.ResolutionMethod;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,13 +27,13 @@ public class BeerController {
 		this.beerService = beerService;
 	}
 	
-	@RequestMapping("/{beerId}")
+	@GetMapping("/{beerId}")
 	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
 		return new ResponseEntity<BeerDto>(beerService.getBeerById(beerId), HttpStatus.OK);
 	}
 	
 	@PostMapping
-	public ResponseEntity<BeerDto> handlePost(BeerDto beerDto) {
+	public ResponseEntity<BeerDto> handlePost(@RequestBody BeerDto beerDto) {
 
 		BeerDto savedBeerDto = beerService.saveNewBeer(beerDto);
 		HttpHeaders headers = new HttpHeaders();
@@ -42,7 +44,7 @@ public class BeerController {
 	}
 	
 	@PutMapping("/{beerId}")
-	public ResponseEntity<BeerDto> handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto){
+	public ResponseEntity<BeerDto> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
 		beerService.updateBeer(beerId, beerDto);
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
